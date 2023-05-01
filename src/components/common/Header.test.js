6c52fdb4d4e35eb404d86feb2c8eff0847266c30
Jackie Zhang <viperking@163.com>
2023-05-01 17:43:38 +0800

    fix test cases

--- src/components/common/Header.test.js
+++ src/components/common/Header.test.js
@@ -5,16 +5,18 @@ import { MemoryRouter } from "react-router-dom";
 
 // Note how with shallow render you search for the React component tag
 it("contains 3 NavLinks via shallow", () => {
-  const numLinks = shallow(<Header />).find("NavLink").length;
+  const user = { userName: "test" };
+  const numLinks = shallow(<Header user={user} />).find("NavLink").length;
   expect(numLinks).toEqual(3);
 });
 
 // Note how with mount you search for the final rendered HTML since it generates the final DOM.
 // We also need to pull in React Router's memoryRouter for testing since the Header expects to have React Router's props passed in.
 it("contains 3 anchors via mount", () => {
+  const user = { userName: "test" };
   const numAnchors = mount(
     <MemoryRouter>
-      <Header />
+      <Header user={user} />
     </MemoryRouter>
   ).find("a").length;
 
